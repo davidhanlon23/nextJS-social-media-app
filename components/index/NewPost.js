f3fcd6dc43831e5bab30c8ee36f60a8e4144f3f3
David Hanlon <david.hanlon@sbdinc.com>
2019-08-21 15:43:08 -0400

    added post to feed... working on displaying posts

--- components/index/NewPost.js
+++ components/index/NewPost.js
@@ -1,18 +1,84 @@
-// import Card from "@material-ui/core/Card";
-// import Typography from "@material-ui/core/Typography";
-// import CardHeader from "@material-ui/core/CardHeader";
-// import CardContent from "@material-ui/core/CardContent";
-// import CardActions from "@material-ui/core/CardActions";
-// import Button from "@material-ui/core/Button";
-// import TextField from "@material-ui/core/TextField";
-// import Avatar from "@material-ui/core/Avatar";
-// import IconButton from "@material-ui/core/IconButton";
-// import AddAPhoto from "@material-ui/icons/AddAPhoto";
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable no-unused-vars */
+
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import AddAPhoto from "@material-ui/icons/AddAPhoto";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const NewPost = () => <div>NewPost</div>;
 
-const styles = theme => ({
+const NewPost = ( { classes,auth,text,image,handleChange, handleAddPost, isAddingPost } )=>{
+  return(
+    
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={<Avatar src={auth.user.avatar}/>}
+        title={<Typography variant ="h6" component="h2">{auth.user.name}</Typography>}
+        className={classes.cardHeader}
+      />
+      <CardContent className={classes.cardContent}>
+        <TextField 
+          label="Add a status"
+          value={text}
+          name="text"
+          multiline
+          row="2"
+          placeholder={`What's on your mind, ${auth.user.name}?`}
+          fullWidth
+          margin="normal"
+          onChange={handleChange}
+          variant="outlined"
+          InputLabelProps={{ shrink:true }}
+        />
+
+        <input 
+          accept="image/*"
+          name="image"
+          id="image"
+          onChange={handleChange}
+          className={classes.input}
+          type="file"
+
+        />
+
+        <label htmlFor="image">
+          <IconButton
+            color="secondary"
+            component="span"
+          >
+            <AddAPhoto/>  
+          </IconButton>
+
+        </label>
+        <span>{image && image.name}</span>
+      </CardContent>
+
+      <CardActions className={classes.cardActions}>
+        <Button
+          color="primary"
+          variant="contained"
+          disabled={!text || isAddingPost}
+          className={classes.submit}
+          onClick ={handleAddPost}
+        >
+          {isAddingPost? "Posting": "Post"}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+  
+ 
+
+const styles = theme => ( {
   card: {
     marginBottom: theme.spacing.unit * 3,
     backgroundColor: theme.palette.primary.light
@@ -20,6 +86,7 @@ const styles = theme => ({
   cardContent: {
     backgroundColor: "white"
   },
+
   input: {
     display: "none"
   },
@@ -27,6 +94,6 @@ const styles = theme => ({
     display: "flex",
     flexDirection: "row-reverse"
   }
-});
+} );
 
-export default withStyles(styles)(NewPost);
+export default withStyles( styles )( NewPost );
--- components/index/PostFeed.js
+++ components/index/PostFeed.js
@@ -1,21 +1,85 @@
-// import Typography from "@material-ui/core/Typography";
+/* eslint-disable linebreak-style */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable indent */
+/* eslint-disable no-unused-vars */
+
+import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
+import NewPost from "./NewPost";
+import Post from "./Post";
+import { addPost } from "../../lib/api";
 
+// eslint-disable-next-line no-undef
 class PostFeed extends React.Component {
-  state = {};
+  state = {
+    posts:[],
+    text:"",
+    image:"",
+    isAddingPost:false
+  };
 
+  componentDidMount(){
+    // eslint-disable-next-line no-undef
+    this.postData = new FormData();
+  }
+  handleChange = event =>{
+  	let inputValue;
+  	if( event.target.name ==="image" ){
+  		inputValue = event.target.files[0];
+  	
+	  }
+	  else{
+		  inputValue= event.target.value;
+
+	  }
+	  this.postData.set( event.target.name, inputValue );
+	  this.setState( { [event.target.name]: inputValue } );
+
+  }
+
+  handleAddPost =()=>{
+    const { auth } = this.props;
+    this.setState( { isAddingPost:true } );
+    addPost( auth.user._id, this.postData )
+      .then( postData =>{
+        const updatedPosts = [ postData, ...this.state.posts ];
+        this.setState( { posts:updatedPosts,isAddingPost:false, text:"",image:"" } );
+        this.postData.delete( "image" );
+      } )
+      .catch( err=>{
+        console.log( err );
+        this.setState( { isAddingPost:false } );
+      } );
+  }
   render() {
-    return <div>PostFeed</div>;
+    const { classes,auth } = this.props;
+    const { text,image, isAddingPost } = this.state;
+    return (
+      <div className={classes.root}>
+        <Typography variant="h4" component="h1" align="center" color="primary" className={classes.title}>
+          Post Feed
+        </Typography>
+        <NewPost
+          auth={auth}
+          text={text}
+          image={image}
+          handleChange={this.handleChange}
+          isAddingPost={isAddingPost}
+          handleAddPost={this.handleAddPost}
+        />
+        {/* Post List */}
+      </div>
+    );
   }
 }
 
-const styles = theme => ({
+const styles = theme => ( {
   root: {
     paddingBottom: theme.spacing.unit * 2
   },
   title: {
     padding: theme.spacing.unit * 2
   }
-});
+} );
 
-export default withStyles(styles)(PostFeed);
+export default withStyles( styles )( PostFeed );
