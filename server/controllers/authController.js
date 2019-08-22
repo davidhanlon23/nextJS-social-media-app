424d084cec80bf0d6101c7f6d9442b2690acbfa7
David Hanlon <david.hanlon@sbdinc.com>
2019-08-22 16:17:37 -0400

    preference changes to site

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,7 +10,7 @@ exports.validateSignup = ( req, res, next ) => {
 	req.sanitizeBody( "password" );
 
 	req.checkBody( "name","Enter a name" ).notEmpty();
-	req.checkBody( "name", "Name must be between 4 and 10 characters" ).isLength( { min: 4, max: 10 } );
+	req.checkBody( "name", "Name must be between 4 and 10 characters" ).isLength( { min: 4, max: 20 } );
 	req.checkBody( "email", "Enter a valid email" ).isEmail().normalizeEmail();
 
 	req.checkBody( "password", "Enter a password" ).notEmpty();
